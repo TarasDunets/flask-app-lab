817bc08de9dd6c533e4324370be471b706c21630
TarasDunets <taras.dunets.23@pnu.edu.ua>
2024-10-25 14:11:30 +0300

    created users blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -7,3 +7,6 @@ app.config.from_pyfile("../config.py")
 from . import views
 from .posts import post_bp
 app.register_blueprint(post_bp)
+
+from .users import users_bp
+app.register_blueprint(users_bp)
---
+++ app/users/__init__.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+
+users_bp = Blueprint("users",
+                      __name__, 
+                      url_prefix="/users", 
+                      template_folder = "templates/users")
+
+from . import views
