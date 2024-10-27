0d010a935a88c89f825be996c5cfd5a07381637b
TarasDunets <taras.dunets.23@pnu.edu.ua>
2024-10-27 17:39:19 +0200

    added tests

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import (Flask, 
         request, url_for, render_template, abort)
 
+from .posts import post_bp
+from .users import users_bp
+
 app = Flask(__name__)
 app.config.from_pyfile("../config.py")
 
-from . import views
-from .posts import post_bp
 app.register_blueprint(post_bp)
-
-from .users import users_bp
 app.register_blueprint(users_bp)
+
+from . import views
