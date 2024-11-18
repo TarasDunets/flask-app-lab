5f73d79083fb4fd0a91c398e7ded5d5be6fc4395
TarasDunets <taras.dunets.23@pnu.edu.ua>
2024-11-18 21:29:41 +0200

    added second half of wtf-lab

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
-from flask import (Flask, 
-        request, url_for, render_template, abort)
+from flask import Flask
 
-from .posts import post_bp
-from .users import users_bp
+def create_app(config_name="config"):
+  app = Flask(__name__)
+  app.config.from_object(config_name)  # Налаштування з об'єкта
 
-app = Flask(__name__)
-app.config.from_pyfile("../config.py")
+  with app.app_context():
+    from .posts import post_bp
+    from .users import users_bp
+    app.register_blueprint(post_bp)
+    app.register_blueprint(users_bp)
+    from . import views
 
-app.register_blueprint(post_bp)
-app.register_blueprint(users_bp)
-
-from . import views
+  return app
