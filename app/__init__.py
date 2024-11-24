51d715390d9fa1cee7e778dd8705eb073981add2
TarasDunets <taras.dunets.23@pnu.edu.ua>
2024-11-24 19:59:33 +0200

    Lab 7 task 1-9

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+migrate = Migrate()
 
 def create_app(config_name="config"):
   app = Flask(__name__)
   app.config.from_object(config_name)  # Налаштування з об'єкта
 
+  db.init_app(app)
+  migrate.init_app(app, db)
+
   with app.app_context():
+    from .posts import models
     from .posts import post_bp
     from .users import users_bp
     app.register_blueprint(post_bp)
